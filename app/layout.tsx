9192e1d98c1f4b8af4ac179c1557bea87e303206
Gerry Ferlanno Hasan Basri <gerryfhb@gmail.com>
2026-02-23 16:40:46 +0700

    Change Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,17 @@
 import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title:{
+    default: 'YPv2',
+    template: '%s | YPv2'
+  },
+  description: 'Yard Monitoring & Inventory',
+  icons:{
+    icon: '/favicon.ico',
+  }
+}
 
 export default function RootLayout({
   children,
